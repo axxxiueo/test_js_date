7e5781f4490e6caf98db330581e2547a2877c466
axxxiueo <77669299+axxxiueo@users.noreply.github.com>
2023-10-07 08:43:30 +0900

    completed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const today = new Date();
 console.log(today);
-days = ["日","月","火","水","木","金","土"]
+days = ["日","月","火","水","木","金","土"];
 
 // console.log(today.getFullYear())
 // 月は、0が1月、1が2月・・・なので、+1する
@@ -15,9 +15,31 @@ days = ["日","月","火","水","木","金","土"]
 // console.log(days[today.getDay()])
 
 
-let outputText = "今は"
-// outputText = outputText + today.getFullYear() + "年"
-outputText += "「"today.getFullYear() + "年"
+// let outputText = "今は";
+// outputText = outputText + today.getFullYear() + "年";
+// outputText += "「" + today.getFullYear() + "年";
+// outputText += (today.getMonth()+1) + "月";
+// outputText += (today.getDate()) + "日";
+// outputText += (today.getHours()) + "時";
+// outputText += (today.getSeconds()) + "秒";
+// outputText += "」です。" ;
+// document.getElementById("date").textContent = outputText;
 
 
-document.getElementById("date").textContent = outputText;
+function outputDate (date) {
+    let  outputText = "「" + date.getFullYear() + "年";
+    outputText += (date.getMonth()+1) + "月";
+    outputText += (date.getDate()) + "日";
+    outputText += (date.getHours()) + "時";
+    outputText += (date.getSeconds()) + "秒";
+    outputText += "」です。" ;
+    return outputText;
+    }
+
+document.getElementById("date").textContent = "今は" + outputDate(today);
+
+const targetDate = new Date(2024, 9 - 1, 27);
+document.getElementById("countDown").textContent = "誕生日まであと" + Math.ceil((targetDate - today) / (1000 * 60 * 60 * 24)) + "日";
+
+today.setDate(today.getDate()+1);
+document.getElementById("tomorrow").textContent = "明日の今は" + outputDate(today);
